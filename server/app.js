a4b2dcfff310cd0b21a2e93fb8aae65415bffde8
01Arsalan <109332699+01Arsalan@users.noreply.github.com>
2024-07-22 15:09:58 +0530

    Update app.js "secure: true-"

--- server/app.js
+++ server/app.js
@@ -52,7 +52,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: true, // Set to true if using HTTPS
+    secure: false, // Set to true if using HTTPS
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
